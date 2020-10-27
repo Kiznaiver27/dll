db24c547aa7ab387803e0f524e0bf6b73b5a6434
Kiznaiver27 <60893142+Kiznaiver27@users.noreply.github.com>
2020-10-27 14:39:37 +0100

    primer dll

---
+++ Assets/ProbarDLL.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+public class ProbarDLL : MonoBehaviour
+{
+    private Utils.MyUtils util;
+    // Start is called before the first frame update
+    void Start()
+    {
+        util = new MyUtils();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Mi nombre es " + util.miNombre());
+    }
+}
